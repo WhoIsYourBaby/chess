8fffdade54d0634a4740577a1b26f1e1d6e49f0a
kimoji <376438624@qq.com>
2017-05-31 22:34:52 +0800

    douniu room

--- game-server/app/game/DouniuRoom.js
+++ game-server/app/game/DouniuRoom.js
@@ -1,7 +1,10 @@
+//百人牛牛
 //绑定的room
 var DouniuRoom = function(channel) {
   this.channel = channel;
   this.userList = [];
+  this.maxWillWait = 5; //sec
+  this.willWait = 0;
 };
 
 module.exports = DouniuRoom;
@@ -21,5 +24,33 @@ DouniuRoom.prototype.kickUser = function(usermodel) {
   }
 };
 
-//开始游戏，给所有人发牌
-DouniuRoom.prototype.startGame = function() {};
+//开始游戏，倒计时等待下注
+//发5副牌，其中一个庄家
+//房间所有玩家向非庄家下注
+DouniuRoom.prototype.startGame = function() {
+  this.willWait = this.maxWillWait;
+  this.willStartTimer = setInterval(this.willStartTimerCall.bind(this), 1000);
+};
+
+DouniuRoom.prototype.willStartTimerCall = function() {
+  this.willWait --;
+  this.pushWillStartMessage();
+  if (this.willWait == 0) {
+    clearTimeout(this.willStartTimer);
+    this.dealPokers();
+  }
+};
+
+
+//state: 0,下注时间等待开始 | 1,游戏开始计算输赢 | 2,其他场景
+DouniuRoom.prototype.pushWillStartMessage = function() {
+  this.channel.pushMessage('brnn.willstart', {
+    code : 200,
+    msg : "下注时间 this.willWait秒",
+    state : 0,
+  });
+}
+
+//给所有人push发牌结果消息
+DouniuRoom.prototype.dealPokers = function() {
+};
