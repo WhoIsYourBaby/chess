59103079af0e3dd81a33a9940b85d6dfd9ff5a83
Mac <Mac@192.168.0.106>
2017-07-10 22:37:31 +0800

    amination poker

---
+++ client_unity/Assets/scripts/ui/PokerItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokerItem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void moveOver(){
+		Debug.Log ("move over");
+	}
+
+	public void flipHalf() {
+		Debug.Log ("flip hafl");
+	}
+}
