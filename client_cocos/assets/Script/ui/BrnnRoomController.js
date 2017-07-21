a956edbeaa02e18551354cc8162b446d276bc7d2
halloworld <li376438624@gmail.com>
2017-07-21 16:15:38 +0800

    add brnnroom

--- client_cocos/assets/Script/ui/BrnnRoomController.js
+++ client_cocos/assets/Script/ui/BrnnRoomController.js
@@ -1,4 +1,5 @@
 require("../pomelo/pomelo-client");
+var GateConnector = require("../protocol/GateConnector");
 
 cc.Class({
     extends: cc.Component,
@@ -14,14 +15,25 @@ cc.Class({
         //    readonly: false,    // optional, default is false
         // },
         // ...
+        buttonExit: {
+            default: null,
+            type: cc.Button
+        },
     },
 
     // use this for initialization
     onLoad: function () {
+        this.buttonExit.node.on('click', this.buttonExitTap, this);
     },
 
     // called every frame, uncomment this function to activate update callback
     // update: function (dt) {
 
     // },
+
+    buttonExitTap: function(){
+        GateConnector.connectorExit(function() {
+            cc.director.loadScene('Home');
+        });
+    }
 });
