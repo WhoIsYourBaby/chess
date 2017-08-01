dfa553e574646dfc8e6c8b871ce9aa460a3e0041
halloworld <li376438624@gmail.com>
2017-08-01 17:56:23 +0800

    返回游戏的房间信息

---
+++ game-server/app/game/RoomManager.js
@@ -0,0 +1,8 @@
+var RoomManager = function() {};
+
+module.exports = RoomManager;
+
+
+//获取限制个数的游戏房间信息
+RoomManager.fetchRoomInfo = function(sqlHelper, roomTable, callback) {
+};
--- game-server/app/servers/connector/handler/entryHandler.js
+++ game-server/app/servers/connector/handler/entryHandler.js
@@ -1,87 +1,54 @@
 
 var UToken = require('../../../game/UToken.js');
 var GMResponse = require('../../../game/GMResponse.js');
+var RoomManager = require('../../../game/RoomManager.js');
 
-module.exports = function(app) {
+module.exports = function (app) {
 	return new Handler(app);
 };
 
-var Handler = function(app) {
-		this.app = app;
+var Handler = function (app) {
+	this.app = app;
 };
 
 var handler = Handler.prototype;
-
-/**
- * New client entry brnn server.
- *
- * @param  {Object}   msg     request message{userid, rid}
- * @param  {Object}   session current session object
- * @param  {Function} next    next stemp callback
- * @return {Void}
- */
-handler.enter = function(msg, session, next) {
-	var rid = msg.rid;
-	var uid = msg.userid + '*' + rid;
-	var sessionService = this.app.get('sessionService');
-
-	//duplicate log in
-	if(!sessionService.getByUid(uid)) {
-		session.bind(uid);
-		session.set('rid', rid);
-		session.push('rid', function(err) {
-			if(err) {
-				console.error('set rid for session service failed! error is : %j', err.stack);
-			}
-		});
-		session.on('closed', onUserLeave.bind(null, this.app));
-	}
-
-	//put user into channel
-	this.app.rpc.brnn.brnnRemote.add(session, uid, this.app.get('serverId'), rid, true, function(users){
-		next(null, {
-			users:users
-		});
-	});
-};
-
 
 /*
 msg.rid		房间id(百人类型的大房间可以不需要rid，因为只有一个这样的房间)
 msg.rtype	房间类型：brnn（百人牛牛）
 msg.token	必须有效的token才能建立session
  */
-handler.enterRoom = function(msg, session, next) {
+handler.enterRoom = function (msg, session, next) {
 	var tokenStr = msg.token;
 	var token = new UToken();
 	token.decrypt(tokenStr);
 	var sessionService = this.app.get('sessionService');
 	if (token.isValid() == false) {
 		next(null, {
-			code : -102,
-			msg : '无效的token'
+			code: -102,
+			msg: '无效的token'
 		});
 		sessionService.kickBySessionId(session.id);
-		return ;
+		return;
 	}
 
 	if (msg.rtype == 'brnn') {
 		var rid = msg.rtype;		//brnn不需要rid，用rtype作为rid
 		//duplicate log in
-		if(!sessionService.getByUid(token.userid)) {
+		if (!sessionService.getByUid(token.userid)) {
 			session.bind(token.userid);
 			session.set('rid', rid);
-			session.push('rid', function(err) {
-				if(err) {
+			session.push('rid', function (err) {
+				if (err) {
 					console.error('set rid for session service failed! error is : %j', err.stack);
 				}
 			});
 			session.on('closed', brnnOnUserLeave.bind(null, this.app));
 		}
 		//put user into channel
-		this.app.rpc.brnn.brnnRemote.add(session, token.userid, this.app.get('serverId'), msg.rtype, true, function(users){
+		this.app.rpc.brnn.brnnRemote.add(session, token.userid, this.app.get('serverId'), msg.rtype, true, function (users) {
 			next(null, {
-				users:users
+				users: users
 			});
 		});
 	}
@@ -94,24 +61,45 @@ handler.enterRoom = function(msg, session, next) {
  * @param {Object} session current session object
  *
  */
-//废弃的方法
-var onUserLeave = function(app, session) {
-	if(!session || !session.uid) {
-		return;
-	}
-	app.rpc.brnn.brnnRemote.exit(session, session.uid, app.get('serverId'), session.get('rid'), null);
-};
-
-var brnnOnUserLeave = function(app, session) {
-	if(!session || !session.uid) {
+var brnnOnUserLeave = function (app, session) {
+	if (!session || !session.uid) {
 		return;
 	}
 	app.rpc.brnn.brnnRemote.exit(session, session.uid, app.get('serverId'), session.get('rid'), null);
 };
 
 
-handler.exit = function(msg, session, next) {
-	this.app.rpc.brnn.brnnRemote.exit(session, session.uid, this.app.get('serverId'), session.get('rid'), function(res) {
+handler.exit = function (msg, session, next) {
+	this.app.rpc.brnn.brnnRemote.exit(session, session.uid, this.app.get('serverId'), session.get('rid'), function (res) {
 		next(null, res);
 	});
+};
+
+
+/**
+ * for 多个游戏选择
+ */
+
+/*
+获取不同游戏的房间信息
+msg.rtype	房间类型：jdnn（经典牛牛），zjh（扎金花），bjl（百家乐）
+msg.token	必须有效的token才能建立session
+ */
+handler.fetchRoomInfo = function (msg, session, next) {
+	var tokenStr = msg.token;
+	var token = new UToken();
+	token.decrypt(tokenStr);
+	var sessionService = this.app.get('sessionService');
+	if (token.isValid() == false) {
+		next(null, {
+			code: -102,
+			msg: '无效的token'
+		});
+		sessionService.kickBySessionId(session.id);
+		return;
+	}
+
+	var roomTableName = 't_room_' + msg.rtype;
+	var sqlHelper = this.app.get('sqlHelper');
+	RoomManager.fetchRoomInfo();
 };
