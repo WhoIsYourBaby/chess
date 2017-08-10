65898e3fcf98ba8044d6438b092e974885c99024
halloworld_mbp <halloworld@mbp.com>
2017-08-10 21:15:38 +0800

    游戏逻辑流程

--- game-server/app/game/JdnnRoom.js
+++ game-server/app/game/JdnnRoom.js
@@ -13,7 +13,12 @@ var JdnnRoom = function (channel, sqlHelper, roomdata) {
 
   this.readyList = {};
 
-  this.state = 0;   //state: 0,下注时间等待开始 | 1,游戏开始计算输赢 | 2,空闲场景
+  //0、产生庄家
+  //1、非庄家选择分数倍数
+  //2、发牌开始
+  //3、开牌
+  //4、空闲时间
+  this.state = 0;
 };
 
 module.exports = JdnnRoom;
@@ -28,26 +33,23 @@ JdnnRoom.prototype.joinUser = function (userid) {
   } else {
     this.roomdata.users += (userid);
   }
-  this.roomdata.usercount ++;
+  this.roomdata.usercount++;
   this.readyList[userid] = 0; //不用bool而用int
   //sql update
-  var sqlstring = "update t_room set usercount = '" + this.roomdata.usercount 
-  + "', users = '" + this.roomdata.users 
-  + "' where roomid = '" + this.roomdata.roomid + "';";
-  this.sqlHelper.query(sqlstring, null, function (error, results, fields){
-    if (error) {
-      console.error(error);
-    }
-  });
+  this.updateUserInSQL();
   return true;
 };
 
+
+//判断用户是否加入了该房间
 JdnnRoom.prototype.hasUser = function (userid) {
   if (this.roomdata.users.indexOf(userid) < 0) {
     return false;
   } else return true;
 };
 
+
+//玩家准备状态修改
 JdnnRoom.prototype.userReady = function (userid, ready) {
   if (this.hasUser(userid) == false) {
     return null;
@@ -77,9 +79,93 @@ JdnnRoom.prototype.checkAllReady = function () {
   return true;
 };
 
-
+//玩家离开
 JdnnRoom.prototype.exitUser = function (userid, serverid) {
+  //把字符串变为
   delete this.readyList[userid];
   var uarr = this.roomdata.users.split(',');
-  
+  for (var index = 0; index < uarr.length; index++) {
+    var element = uarr[index];
+    if (element == userid) {
+      uarr.splice(index, 1);
+    }
+  }
+  this.roomdata.users = uarr.join(',');
+  this.roomdata.usercount = uarr.length;
+  this.updateUserInSQL();
+  this.channel.leave(userid, serverid);
+};
+
+//更新数据库中该房间的用户信息
+JdnnRoom.prototype.updateUserInSQL = function () {
+  //sql update
+  var sqlstring = "update t_room set usercount = '" + this.roomdata.usercount
+    + "', users = '" + this.roomdata.users
+    + "' where roomid = '" + this.roomdata.roomid + "';";
+  this.sqlHelper.query(sqlstring, null, function (error, results, fields) {
+    if (error) {
+      console.error(error);
+    }
+  });
+};
+
+//重置各种数据为下一把做准备
+JdnnRoom.prototype.reset = function () {
+  this.chipList = {};
+  this.state = 0;
+};
+
+//state
+//0、准备
+//1、产生庄家
+//2、非庄家选择分数倍数
+//3、发牌开始
+//4、开牌 + 结算
+//5、空闲时间
+//->0
+JdnnRoom.prototype.startGame = function () {
+  this.reset();
+  this.gamePrepare();
+};
+
+JdnnRoom.prototype.gamePrepare = function () {
+  var self = this;
+  var secPre = 3;
+  var prepareTimer = setInterval(function () {
+    var param = { state: 0, time: secPre };
+    var response = new GMResponse(1, '准备时间', param);
+    self.channel.pushMessage('jdnn.gamePrepare');
+    //准备时间过后开始确定庄家
+    secPre--;
+    if (secPre <= 0) {
+      clearInterval(prepareTimer);
+      //开始选择庄家
+      self.gameMarkBanker();
+    }
+  }, 1000);
+};
+
+//随机产生庄家
+JdnnRoom.prototype.gameMarkBanker = function () {
+  var uarr = this.roomdata.users.split(',');
+  var bankerIndex = parseInt(Math.random() * uarr.length);
+  var bankerUid = uarr[bankerIndex];
+  var response = new GMResponse(1, '标记庄家', { 'banker': bankerUid });
+  this.channel.pushMessage('jdnn.markBanker', response);
+
+  this.gameChip();
+};
+
+//选择分数倍数
+JdnnRoom.prototype.gameChip = function () {
+  var self = this;
+  var secChip = 3;
+  var chipTimer = setInterval(function () {
+
+    secChip --;
+    if (secChip <= 0) {
+      clearInterval(chipTimer);
+      //todo发牌
+    }
+  }, 1000);
 };
