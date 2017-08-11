777925020e5dfcb7a6c7493128d41a5fd26f53f2
halloworld_mbp <halloworld@mbp.com>
2017-08-11 19:15:16 +0800

    等计算结果了

--- game-server/app/game/PokerManager.js
+++ game-server/app/game/PokerManager.js
@@ -66,3 +66,120 @@ PokerManager.prototype.dealSomePoker = function(count) {
     this.pokers.splice(-count, count);
     return somepk;
 }
+
+
+//static method
+
+//for 牛牛
+//计算牌面大小
+
+//pokers -> 5张牌数组
+PokerManager.nnResultForPoker = function(pokers) {
+  //1 遍历所有元素，设置nnValue(大于10都设置为10)
+  //顺便统计五花、四花(>10)、五小(<10)、炸弹条件满足情况
+  var total = 0;
+  //nntype表示用户牌型
+  //炸弹(6) > 五小(5) > 五花(4) > 四花(3) > 牛牛(2) > 有分(1) > 没分(0)
+  var nntype = 0;
+  var wuxiaoCount = 0;  //小于5的牌张数
+  var zhadanDic = {};
+  var tenCount = 0;     //等于10的牌张数
+  var huaCount = 0;     //大于10的牌张数
+  for (var index = 0; index < pokers.length; index++) {
+    var pk = pokers[index];
+    pk.nnValue = pk.value > 10 ? 10 : pk.value;
+    total += pk.nnValue;
+
+    zhadanDic[pk.value] = ((zhadanDic[pk.value] == undefined) ? 1 : zhadanDic[pk.value] + 1);
+
+    if (pk.value == 10) {
+      tenCount ++;
+    }
+
+    if (pk.value > 10) {
+      huaCount ++;
+    }
+  }
+
+  var res = {};
+  //2 优先判断炸弹
+  for (var key in zhadanDic) {
+    if (zhadanDic.hasOwnProperty(key)) {
+      var element = zhadanDic[key];
+      if (element == 4) {
+        nntype = 6;
+        res.nntype = nntype;
+        res.niuN = key;     //炸弹的时候，niuN的值是炸弹牌
+        res.pIndex1 = -1;
+        res.pIndex2 = -1;
+        return res;
+      }
+    }
+  }
+
+  //3 判断五小
+  if (wuxiaoCount == 5 && total <= 10) {
+    nntype = 5;
+    res.nntype = nntype;
+    res.niuN = 0;     //五小的时候为0
+    res.pIndex1 = -1;
+    res.pIndex2 = -1;
+    return res;
+  }
+
+  //4 判断五花
+  if (huaCount == 5) {
+    nntype = 4;
+    res.nntype = nntype;
+    res.niuN = 0;     //五小的时候为0
+    res.pIndex1 = -1;
+    res.pIndex2 = -1;
+    return res;
+  }
+
+  //5 判断四花
+  if (huaCount == 4 && tenCount == 1) {
+    nntype = 3;
+    res.nntype = nntype;
+    res.niuN = 0;     //五小的时候为0
+    res.pIndex1 = -1;
+    res.pIndex2 = -1;
+    return res;
+  }
+
+  //6 判断牛牛
+  var niuN = total % 10;
+  var hasNiu = false;
+  
+  for (var index = 0; index < pokers.length; index++) {
+    for (var sec = index + 1; sec < pokers.length; sec++) {
+      var pkf = pokers[index];
+      var pkl = pokers[sec];
+      var testN = (pkf.nnValue + pkl.nnValue) % 10;
+      if (testN == niuN) {
+        hasNiu = true;
+        if (niuN == 0) {
+          res.nntype = 2;
+        } else {
+          res.nntype = 1;
+        }
+        res.niuN = niuN;
+        res.pIndex1 = index;
+        res.pIndex2 = sec;
+        break ;
+      }
+    }
+    if (hasNiu) {
+      break ;
+    }
+  }
+
+  //没牛的情况
+  if (!hasNiu) {
+    res.nntype = 0;
+    res.niuN = -1;
+    res.pIndex1 = -1;
+    res.pIndex2 = -1;
+  }
+  return res;
+}
