3db662b9c661a9383803e615f28c320238aa4648
halloworld_mbp <halloworld@mbp.com>
2017-08-14 18:23:35 +0800

    tiaozheng connector

--- game-server/app/servers/gate/handler/gateHandler.js
+++ game-server/app/servers/gate/handler/gateHandler.js
@@ -1,11 +1,11 @@
 var UToken = require('../../../game/UToken.js');
 var GMResponse = require('../../../game/GMResponse.js');
 
-module.exports = function(app) {
+module.exports = function (app) {
 	return new Handler(app);
 };
 
-var Handler = function(app) {
+var Handler = function (app) {
 	this.app = app;
 };
 
@@ -19,9 +19,9 @@ var handler = Handler.prototype;
  * @param {Function} next next stemp callback
  *
  */
-handler.queryEntry = function(msg, session, next) {
+handler.queryEntry = function (msg, session, next) {
 	var userid = msg.userid;
-	if(!userid) {
+	if (!userid) {
 		next(null, {
 			code: 500
 		});
@@ -29,7 +29,7 @@ handler.queryEntry = function(msg, session, next) {
 	}
 	// get all connectors
 	var connectors = this.app.getServersByType('connector');
-	if(!connectors || connectors.length === 0) {
+	if (!connectors || connectors.length === 0) {
 		next(null, {
 			code: 500
 		});
@@ -45,21 +45,21 @@ handler.queryEntry = function(msg, session, next) {
 };
 
 
-handler.guestLogin = function(msg, session, next){
+handler.guestLogin = function (msg, session, next) {
 	var sqlHelper = this.app.get('sqlHelper');//获取全局mysql client
-	sqlHelper.guestLogin(function(err, userinfo){
+	sqlHelper.guestLogin(function (err, userinfo) {
 		console.log(userinfo);
 		var token = new UToken(userinfo.userid).encrypt();
 		if (err) {
 			//失败
 			next(null, {
-				code : -101,
-				msg : '游客登录失败，请重试'
+				code: -101,
+				msg: '游客登录失败，请重试'
 			});
 		} else {
 			// get all connectors
 			var connectors = this.app.getServersByType('connector');
-			if(!connectors || connectors.length === 0) {
+			if (!connectors || connectors.length === 0) {
 				var response = new GMResponse(-103, '没有找到connector');
 				next(null, response);
 				return;
@@ -67,14 +67,11 @@ handler.guestLogin = function(msg, session, next){
 			// here we just start `ONE` connector server, so we return the connectors[0] 
 			var res = connectors[0];
 			var data = {
-						userinfo : userinfo,
-						token : token,
-						connector : {
-							host: res.host,
-							// host: '39.108.83.192',
-							port: res.clientPort
-						}
-					};
+				userinfo: userinfo,
+				token: tokenString,
+				localConnector: { host: res.host, port: res.clientPort },
+				remoteConnector: { host: '39.108.83.192', port: res.clientPort },
+			};
 			var response = new GMResponse(1, 'ok', data);
 			next(null, response);
 		}
@@ -82,22 +79,22 @@ handler.guestLogin = function(msg, session, next){
 }
 
 
-handler.refreshToken = function(msg, session, next){
+handler.refreshToken = function (msg, session, next) {
 	var tokenStr = msg.token;
 	var token = new UToken();
 	console.log(tokenStr);
 	token.decrypt(tokenStr);
 	if (token.isValid() == false) {
 		next(null, new GMResponse(-101, '非法token'));
-		return ;
+		return;
 	}
 	token.refresh();
 	var sqlHelper = this.app.get('sqlHelper');
-	sqlHelper.queryUserInfo(token.userid, function(err, userinfo){
+	sqlHelper.queryUserInfo(token.userid, function (err, userinfo) {
 		if (userinfo) {
 			// get all connectors
 			var connectors = this.app.getServersByType('connector');
-			if(!connectors || connectors.length === 0) {
+			if (!connectors || connectors.length === 0) {
 				var response = new GMResponse(-103, '没有找到connector');
 				next(null, response);
 				return;
@@ -106,11 +103,11 @@ handler.refreshToken = function(msg, session, next){
 			var res = connectors[0];
 			var tokenString = token.encrypt();
 			var data = {
-						userinfo : userinfo,
-						token : tokenString,
-						localConnector : {host: res.host,port:res.clientPort},
-						remoteConnector : {host: '39.108.83.192',port:res.clientPort},
-					};
+				userinfo: userinfo,
+				token: tokenString,
+				localConnector: { host: res.host, port: res.clientPort },
+				remoteConnector: { host: '39.108.83.192', port: res.clientPort },
+			};
 			var response = new GMResponse(1, 'ok', data);
 			next(null, response);
 		} else {
